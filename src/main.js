b854bb42f0d6684097e89cdeb6508917dc3560b6
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2024-01-02 15:04:30 -0300

    Criando a view

--- src/main.js
+++ src/main.js
@@ -1,18 +1,7 @@
 import { Personagem } from "./modules/personagem.js";
 
-// const BlazeDraco = new Personagem();
-// BlazeDraco.nome = "BlazeDraco";
-// BlazeDraco.vida = 10;
-// BlazeDraco.mana = 10;
-// BlazeDraco.level = 100;
-// BlazeDraco.tipo = "Mago";
-
 const BlazeDraco = new Personagem("BlazeDraco", 10, 10, 100, "Mago");
-console.log(BlazeDraco);
+const ShadowWing = new Personagem("ShadowWing", 10, 10, 100, "Ladino");
 
-const ShadowWing = new Personagem();
-ShadowWing.nome = "ShadowWing";
-ShadowWing.vida = 10;
-ShadowWing.mana = 10;
-ShadowWing.level = 100;
-ShadowWing.tipo = "Ladino";
+console.log(BlazeDraco);
+console.log(ShadowWing);
