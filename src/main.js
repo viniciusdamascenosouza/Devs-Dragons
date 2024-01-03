b9769afdb0ac97bf22517513762eff8e85b593a6
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2024-01-03 08:23:36 -0300

    Composição

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import { personagemView } from "./components/personagem-view.js";
 import { Mago } from "./modules/mago.js";
 import { Arqueiro } from "./modules/arqueiro.js";
+import { ArqueiroMago } from "./modules/arqueiro-mago.js";
 
 // magos
 const BlazeDraco = new Mago("BlazeDraco", 100, "Fogo", 10, 10);
 const ShadowWing = new Mago("ShadowWing", 45, "Gelo", 10, 10);
 
 // arqueiros
-const ThunderTail = new Arqueiro("ThunderTail", 60, 8);
+const ThunderTail = new Arqueiro("ThunderTail", 60, 8, 7, 9);
 
-const personagens = [BlazeDraco, ShadowWing, ThunderTail];
+// arqueiros magos
+const CelestialChaos = new ArqueiroMago("CelestialChaos", 87, 8, 7, 7, "Raio", 10, 8 )
+
+const personagens = [BlazeDraco, ShadowWing, ThunderTail, CelestialChaos];
 new personagemView(personagens).render();
---
+++ src/modules/arqueiro-mago.js
@@ -0,0 +1,28 @@
+import { Arqueiro } from "./arqueiro.js";
+import { Mago } from "./mago.js";
+import { Personagem } from "./personagem.js";
+
+export class ArqueiroMago extends Personagem {
+  ladoArqueiro;
+  ladoMago;
+  tipo = "ArqueiroMago";
+
+  constructor(
+    nome,
+    level,
+    destreza,
+    agilidade,
+    vigilancia,
+    elementoMagico,
+    levelMagico,
+    inteligencia
+  ) {
+    super(nome, level);
+    this.ladoArqueiro = new Arqueiro(destreza, agilidade, vigilancia);
+    this.ladoMago = new Mago(elementoMagico, levelMagico, inteligencia);
+  }
+
+  obterInsignia() {
+    return `${this.ladoArqueiro.obterInsignia()} e ${this.ladoMago.obterInsignia()}`;
+  }
+}
